7c8967162c8abf26b6c1a23df5dd29544382e8af
sy <greatesj@daum.net>
2019-05-11 13:19:26 +0900

    회원가입 페이지 및 회원가입 폼 저장(미완료)

--- src/page/signup/SignupPage.js
+++ src/page/signup/SignupPage.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import SignupForm from '../../component/signup/SignupForm'
+
+import { Grid } from 'semantic-ui-react'
 
 class SignupPage extends Component {
 
     render() {
         return (
-            <div>
-                SignupPage
-            </div>
+            <Grid>
+                <Grid.Row centered>
+                
+                    <Grid.Column width={8}>
+                        <SignupForm />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         )
     }
 }
