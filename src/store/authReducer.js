2093922bd0ac22660e636277762bc02527a65233
sy <greatesj@daum.net>
2019-05-11 11:52:15 +0900

    authReducer 추가

---
+++ src/store/authReducer.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    user:null,
+}
+
+export default function authReducer(state=initialState, action){
+    switch(action.type){
+
+        default:
+            return state;
+    }
+}
