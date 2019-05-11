8e3d56afed45c0d7abfb23be7516e693dacdfdfd
sy <greatesj@daum.net>
2019-05-11 13:18:48 +0900

    store/ index

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
-import {createStore, combineReducer} from 'redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import authReducer from './authReducer'
+import signupReducer from './signupReducer'
 
 
 export function configureStore(){
     return createStore(
-        combineReducer({
+        combineReducers({
             auth: authReducer
+            signup: signupReducer
         }),
-        applyMiddleWare(thunk)
+        applyMiddleware(thunk)
         )
 }
