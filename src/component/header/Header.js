3aaa9ff7d527264fca04b9320999fe6c8d7403ed
sy <greatesj@daum.net>
2019-05-11 11:36:59 +0900

    헤더 추가

---
+++ src/component/header/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+
+class Header extends Component {
+
+
+    render() {
+        return (
+            <Menu>
+                <Menu.Item header> 
+                  게시판
+                </Menu.Item>
+                    <Menu.Menu position="right">
+                        <Menu.Item>
+                            <Link to="/"> 홈 </Link>
+                        </Menu.Item>
+                    </Menu.Menu>
+                        <Menu.Item>
+                            <Link to="/login"> 로그인 </Link>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <Link to="/signup"> 회원가입 </Link>
+                        </Menu.Item>               
+            </Menu>
+
+        )
+    }
+}
+
+export default Header
